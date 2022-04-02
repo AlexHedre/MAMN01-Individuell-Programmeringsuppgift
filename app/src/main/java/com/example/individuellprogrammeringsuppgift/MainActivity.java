e275b320038cc909fef85e32cc828547a9f0bd63
AlexHedre <alex.hedre@gmail.com>
2022-04-02 17:58:10 +0200

    Added functionality for the compass

--- app/src/main/java/com/example/individuellprogrammeringsuppgift/MainActivity.java
+++ app/src/main/java/com/example/individuellprogrammeringsuppgift/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_MESSAGE = "Individuell programmeringsuppgift Alexander Hedre";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +15,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user taps the Send button */
+    /**
+     * Called when the user taps the Send button
+     */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.textInputEditText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when the user taps the Compass button
+     */
+    public void goToCompass(View view) {
+        Intent intent = new Intent(this, Compass.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when the user taps the Accelerometer button
+     */
+    public void goToAccelerometer(View view) {
+        Intent intent = new Intent(this, Accelerometer.class);
         startActivity(intent);
     }
 }
